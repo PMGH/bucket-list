da3db424f701be05fa22bea41426063b36c54d2f
Clare <chblackburne@gmail.com>
2017-12-13 13:32:23 +0000

    chosenCountry constructor added

---
+++ client/src/models/chosenCountry.js
@@ -0,0 +1,9 @@
+var ChosenCountry = function(name, lat, long, flag){
+    this.name = name;
+    this.lat = lat;
+    this.long = long;
+    this.flag = flag;
+}
+
+
+module.exports = ChosenCountry;
